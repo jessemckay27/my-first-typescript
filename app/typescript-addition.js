5a2d0560df4062dabbdcca12bd2298f114189002
Jesse <rosecity27@comcast.net>
2017-01-19 10:30:12 -0800

    finished lesson

---
+++ app/typescript-addition.js
@@ -0,0 +1,16 @@
+// var firstNumber: number = prompt('Please enter a number.');
+// var secondNumber: number = prompt('Enter another number.');
+// var sum: number = firstNumber + secondNumber;
+// alert("The sum of your two numbers is: " + sum);
+//
+// var firstNumber: number = parseInt(prompt('Please enter a number.'));
+// var secondNumber: number = parseInt(prompt('Enter another number.'));
+// var sum: number = firstNumber + secondNumber;
+// alert("The sum of your two numbers is: " + sum);
+var findSum = function (firstValue, secondValue) {
+    var sum = firstNumber + secondNumber;
+    alert("The sum of your two numbers is: " + sum);
+};
+var firstNumber = prompt('Please enter a number.');
+var secondNumber = prompt('Enter another number.');
+findSum(firstNumber, secondNumber);
---
+++ app/typescript-addition.ts
@@ -0,0 +1,32 @@
+// var firstNumber: number = prompt('Please enter a number.');
+// var secondNumber: number = prompt('Enter another number.');
+// var sum: number = firstNumber + secondNumber;
+// alert("The sum of your two numbers is: " + sum);
+//
+
+
+// var firstNumber: number = parseInt(prompt('Please enter a number.'));
+// var secondNumber: number = parseInt(prompt('Enter another number.'));
+// var sum: number = firstNumber + secondNumber;
+// alert("The sum of your two numbers is: " + sum);
+
+
+// 
+// var findSum = function(firstValue: number, secondValue: number) {
+//   var sum: number = firstNumber + secondNumber;
+//   alert("The sum of your two numbers is: " + sum);
+// }
+//
+// var firstNumber = prompt('Please enter a number.');
+// var secondNumber = prompt('Enter another number.');
+// findSum(firstNumber, secondNumber);
+
+
+var findSum = function(firstValue: number, secondValue: number) {
+  var sum: number = firstNumber + secondNumber;
+  alert("The sum of your two numbers is: " + sum);
+}
+
+var firstNumber = parseInt(prompt('please enter a number.'));
+var secondNumber = parseInt(prompt('enter another number.'));
+findSum(firstNumber, secondNumber);
